330dbc6d7c063127b68443887c9c831cae753914
John Yan <116501930+JohnnyNetsec@users.noreply.github.com>
2023-07-24 14:46:29 -0400

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -26,7 +26,7 @@ const Footer = ({ title }) => {
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} | {BLOG.BIO}</h1>
+        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</h1>
       </span><br/>
   
 <div>51Sec Apps</div>
